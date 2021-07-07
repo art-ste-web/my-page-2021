4f88b0985f36ba536f5b265597d3b0bbe9bcc649
art-ste-web <art.ste.web@gmail.com>
2021-07-07 21:24:26 +0300

    add animated header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
-
-
-//console.log(localStorage);
+const animHeader = document.querySelectorAll('#header-web-d path');
+for(let i = 0; i < animHeader.length; i++) {
+    console.log(`Letter ${i} is ${animHeader[i].getTotalLength()}`);
+}
